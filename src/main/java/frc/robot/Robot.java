b4555b1f79b7e99edd5f8224a1b717a20aa6871a
Artificys <tbyrne423@yahoo.com>
2023-10-27 17:38:53 -0400

    Initial Swerve Push from REV Template

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,247 +4,99 @@
 
 package frc.robot;
 
-import org.photonvision.PhotonCamera;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Drivetrain;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-enum RGBMode {
-	RAINBOW, SHOOT, SHOOT_RAINBOW
-}
-
 public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
 
-	private Command m_autonomousCommand;
-	public static Drivetrain drivetrain;
-	public static boolean orientation = true;
-	private SendableChooser<Command> autoChooser;
-	public static boolean currentcheck;
-	public static Trajectory revgo, go, wcurve, parkReturn, outAndReturn;
+  private RobotContainer m_robotContainer;
 
-	public static Command forward, back, TwoPieceLeft1, TwoPieceLeft2, TwoPieceRight1, TwoPieceRight2;
-	public static String trajectoryJSON = "paths/Joe.wpilib.json";
-	public static PhotonCamera camera;
-	private static Compressor compressor;
-	public PowerDistribution pdp;
-	private Timer wristTimer = new Timer();
-	private boolean wristChange = false;
-	public static double scoringPreset;
-	public static double intakePresetFront;
-	public static double intakePresetBack;
-	public static double stationPreset;
-	public static double midScoringPreset;
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
 
-	@Override
-	public void robotInit() {
-		CameraServer.startAutomaticCapture(0);
-		CameraServer.startAutomaticCapture(1);
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-		pdp = new PowerDistribution(1, ModuleType.kRev);
-		compressor = new Compressor(PneumaticsModuleType.REVPH);
-		drivetrain = new Drivetrain();
-		new RobotContainer();
-		autoChooser = new SendableChooser<>();
-		drivetrain.resetGyro();
-		drivetrain.resetEncoders();
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
 
-		// autoChooser.setDefaultOption("(CENTER) Balance No Mobility", new
-		// MiddleBalanceAuto());
-		// autoChooser.addOption("(CENTER) CUBE + Balance + Mobility", new
-		// CubeBalance());
+  @Override
+  public void disabledPeriodic() {}
 
-		// autoChooser.addOption("(Non-Wire side) Cone + Cube", new ConeCube());
-		// autoChooser.addOption("(RED)(Wire side) Cone Cube", new ConeCubeWire());
-		// autoChooser.addOption("(BLUE)(Wire Side) Cone + Cube", new
-		// ConeCubeWireBLUE());
-		SmartDashboard.putData(autoChooser);
-	}
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-	@Override
-	public void robotPeriodic() {
-		CommandScheduler.getInstance().run();
-		compressor.enableAnalog(60, 110);
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
 
-		// if (intake.intakeMotor.getOutputCurrent() > Constants.IntakeMaxCurrent)
-		// 	RobotContainer.driver.setRumble(RumbleType.kBothRumble, 1);
-		// else
-		// 	RobotContainer.driver.setRumble(RumbleType.kBothRumble, 0);
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
 
-		SmartDashboard.putNumber("PReSSURE", compressor.getPressure());
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
 
-		// if (intake.getWristCurrent() > 40) {
-		// wristTimer.reset();
-		// wristTimer.start();
-		// wristChange = true;
-		// }
-		// if (wristTimer.get() > 3 && wristChange) {
-		// intake.setRotateMotor(0);
-		// }
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
 
-		// if (wristChange && intake.getWristCurrent() < 30) {
-		// wristChange = false;
-		// wristTimer.reset();
-		// }
-	}
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-	/** This function is called once each time the robot enters Disabled mode. */
-	@Override
-	public void disabledInit() {
-		Robot.drivetrain.setBrakeMode();
-		pdp.setSwitchableChannel(false);
-	}
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-	// @Override
-	// public void disabledPeriodic() {
-	// 	led.setMode("rainbow");
-	// }
-
-	/**
-	 * This autonomous runs the autonomous command selected by your
-	 * {@link RobotContainer} class.
-	 */
-	@Override
-	public void autonomousInit() {
-		drivetrain.setRampUp(0);
-
-		pdp.setSwitchableChannel(true);
-		Constants.ManualExtend = true;
-		drivetrain.resetGyro();
-		m_autonomousCommand = autoChooser.getSelected();
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.schedule();
-		}
-	}
-
-	/** This function is called periodically during autonomous. */
-	@Override
-	public void autonomousPeriodic() {
-
-	}
-
-	@Override
-	public void teleopInit() {
-		Constants.ManualExtend = true;
-		drivetrain.setRampUp(0.25);
-		pdp.setSwitchableChannel(true);
-		Robot.drivetrain.setBrakeMode();
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
-	}
-
-	/** This function is called periodically during operator control. */
-	@Override
-	public void teleopPeriodic() {
-		if (drivetrain.getYaw() > -90 && drivetrain.getYaw() < 90)
-			orientation = false;
-		else
-			orientation = true;
-
-		// if (drivetrain.getYaw() > -90 || drivetrain.getYaw() < 90) // Back
-		// orientation = false;
-		// if (drivetrain.getYaw() < -90 || drivetrain.getYaw() > 90) // Front
-		// orientation = true;
-
-		if (Constants.mode) {
-			intakePresetFront = Constants.ConeIntakePivotFront;
-			intakePresetBack = Constants.ConeIntakePivotBack;
-			if (orientation) {
-				scoringPreset = Constants.ConeScoringPivotFront;
-				stationPreset = Constants.NewStationPivotBack;
-				midScoringPreset = Constants.MidConeScoringPivotFront;
-			} else {
-				scoringPreset = Constants.ConeScoringPivotBack;
-				stationPreset = Constants.NewStationPivotFront;
-				midScoringPreset = Constants.MidConeScoringPivotBack;
-			}
-		}
-
-		else {
-			intakePresetFront = Constants.CubeIntakePivotFront;
-			intakePresetBack = Constants.CubeIntakePivotBack;
-			if (orientation) {
-				scoringPreset = Constants.CubeScoringPivotFront;
-				stationPreset = Constants.NewStationPivotBack;
-				midScoringPreset = Constants.MidConeScoringPivotFront;
-
-			} else {
-				scoringPreset = Constants.CubeScoringPivotBack;
-				stationPreset = Constants.NewStationPivotFront;
-				midScoringPreset = Constants.MidConeScoringPivotBack;
-
-			}
-		}
-
-		double thrust = 0;
-		double turn = 0;
-		if (RobotContainer.driver.getRawButton(6))
-			thrust = -Constants.kCreep;
-		else if (RobotContainer.driver.getRawButton(5))
-			thrust = Constants.kCreep;
-		else {
-			thrust = -(RobotContainer.driver.getRawAxis(3) - RobotContainer.driver.getRawAxis(2));
-			if (Math.abs(thrust) < 0.25)
-				thrust = 0;
-			if (thrust > Constants.MaxSpeed)
-				thrust = Constants.MaxSpeed;
-			if (thrust < -Constants.MaxSpeed)
-				thrust = -Constants.MaxSpeed;
-		}
-		if (Math.abs(RobotContainer.driver.getRawAxis(0)) > 0.1)
-			turn = RobotContainer.driver.getLeftX() * Constants.TURN_FACTOR;
-		if (Math.abs(RobotContainer.driver.getRightX()) > 0.12)
-			turn = RobotContainer.driver.getRightX() * -.35;
-
-		if (RobotContainer.driver.getRawButton(2))
-			turn /= 3.5;
-
-		Robot.drivetrain.arcadeDrive(thrust, turn);
-
-		// if (Constants.mode && !Constants.Intaking)
-		// 	Robot.led.setColor(255, 255, 0); // Cone Lights
-		// if (!Constants.mode && !Constants.Intaking)
-		// 	Robot.led.setColor(128, 0, 128); // Cube Lights
-
-	}
-
-	@Override
-	public void testInit() {
-		CommandScheduler.getInstance().cancelAll();
-	}
-
-	/** This function is called periodically during test mode. */
-	@Override
-	public void testPeriodic() {
-	}
-
-	/** This function is called once when the robot is first started up. */
-	@Override
-	public void simulationInit() {
-	}
-
-	/** This function is called periodically whilst in simulation. */
-	@Override
-	public void simulationPeriodic() {
-	}
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,174 +1,122 @@
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import org.photonvision.PhotonCamera;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import java.util.List;
 
-
-import frc.robot.commands.AutoCommands.Level;
-import frc.robot.commands.DrivetrainCommands.ResetGyro;
-import frc.robot.commands.VisionCommands.PhotonAim;
-
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-	public static PhotonCamera camera, camera2;
-	public static CommandXboxController operator, pitstick;
-	public static XboxController driver;
-	private Trigger driverX, driverY, driverA, driverB, driverLB, driverRB, driverLT, driverRT;
-	private Trigger driverDPadUp, driverDPadDown, driverDPadLeft, driverDPadRight, driverLeftStick;
-	private Trigger driverLeftX, driverLeftY, driverRightX, driverRightY, driverStart, driverTinyRight;
+  // The robot's subsystems
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-	private Trigger operatorX, operatorY, operatorA, operatorB, operatorLB, operatorRB, operatorLT,
-			operatorRT, driverBack;
-	private Trigger operatorLeftXPos, operatorLeftXNeg, operatorLeftYNeg, operatorLeftYPos, operatorLeftY,
-			operatorRightX, operatorRightYPos, operatorRightYNeg;;
-	private Trigger operatorDPadUp, operatorDPadDown, operatorDPadLeft, operatorDPadRight,
-			operatorLeftStick, operatorRightStick, operatorTinyRight, operatorTinyLeft;
+  // The driver's controller
+  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-	/**
-	 * The container for the robot. Contains subsystems, OI devices, and commands.
-	 * // * @throws IOException
-	 */
-	public RobotContainer() {
-		driver = new XboxController(0);
-		operator = new CommandXboxController(1);
-		pitstick = new CommandXboxController(2);
-		configureBindings();
-		pitstickBindings();
-		camera = new PhotonCamera("2791photonvision1");
-		camera2 = new PhotonCamera("2791photonvision");
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
 
-		// DRIVER CONTROLS
-		// driverX.toggleOnTrue(new ChangeMode());
-		// driverA.whileTrue(new ConeOutCubeOut());
-		// driverY.whileTrue(new ConeInCubeOut());
-		driverStart.whileTrue(new Level());
+    // Configure default commands
+    m_robotDrive.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> m_robotDrive.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                true, true),
+            m_robotDrive));
+  }
 
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+   * subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+   * passing it to a
+   * {@link JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    new JoystickButton(m_driverController, Button.kR1.value)
+        .whileTrue(new RunCommand(
+            () -> m_robotDrive.setX(),
+            m_robotDrive));
+  }
 
-		// OPERATOR CONTROLS
-		// operatorDPadUp.toggleOnTrue(new ParallelCommandGroup(new PivotArm(0), new ExtendArm(2)));
-		// operatorDPadDown.toggleOnTrue(new ExtendArm(3));
-		// operatorA.toggleOnTrue(new RotateWrist());
-		// operatorRT.toggleOnTrue(new ScoringExtension());
-		// operatorLT.toggleOnTrue(new ExtendArm(Constants.ConeMidExtension));
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // Create config for trajectory
+    TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(DriveConstants.kDriveKinematics);
 
-		// operatorRB.toggleOnTrue(new PivotSet(1, false));
-		// operatorLB.toggleOnTrue(new PivotSet(4, false));
-		// operatorB.toggleOnTrue(new ParallelCommandGroup(new ExtendArm(0.8), new PivotSet(2, false)));
-		// operatorY.toggleOnTrue(new PivotSet(5,false));
-		// // Intake Presets
-		// operatorTinyLeft.toggleOnTrue(
-		// 		new ParallelCommandGroup(new PivotArm(Constants.FlangeIntakePivotBack), new ExtendArm(0.8)));
-		// operatorTinyRight.toggleOnTrue(
-		// 		new ParallelCommandGroup(new PivotArm(Constants.FlangeIntakePivotFront), new ExtendArm(0.8)));
-		// operatorDPadRight.toggleOnTrue(
-		// 		new ParallelCommandGroup(new ExtendArm(0.8), new PivotSet(3, true)));
-		// operatorDPadLeft.toggleOnTrue(
-		// 		new ParallelCommandGroup(new ExtendArm(0.8), new PivotSet(3, false)));
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        config);
 
-		// operatorLeftYNeg.whileTrue(new ManualPivotDown());
-		// operatorLeftYPos.whileTrue(new ManualPivotUp());
-	}
+    var thetaController = new ProfiledPIDController(
+        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-	/**
-	 * Use this method to define your trigger->command mappings. Triggers can be
-	 * created via the
-	 * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-	 * an arbitrary
-	 * predicate, or via the named factories in {@link
-	 * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-	 * {@link
-	 * CommandXboxController
-	 * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-	 * PS4} controllers or
-	 * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-	 * joysticks}.
-	 */
+    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+        exampleTrajectory,
+        m_robotDrive::getPose, // Functional interface to feed supplier
+        DriveConstants.kDriveKinematics,
 
-	private void pitstickBindings() {
-		// pitstick.rightBumper().toggleOnTrue(new PivotArm(Robot.scoringPreset));
-		// pitstick.b().toggleOnTrue(new PivotArm(Robot.stationPreset));
+        // Position controllers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        thetaController,
+        m_robotDrive::setModuleStates,
+        m_robotDrive);
 
-		// pitstick.povDownRight().toggleOnTrue(new PivotArm(Robot.intakePresetFront));
-		// pitstick.povDownLeft().toggleOnTrue(new PivotArm(Robot.intakePresetBack));
+    // Reset odometry to the starting pose of the trajectory.
+    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
 
-		// pitstick.rightBumper().toggleOnTrue(new	PivotArm(Constants.ConeScoringPivotFront));
-		// pitstick.leftBumper().toggleOnTrue(new PivotArm(Constants.ConeScoringPivotBack));
-		// pitstick.rightTrigger().toggleOnTrue(new ExtendArm(Constants.ConeUpperExtension));
-		// pitstick.leftTrigger().toggleOnTrue(new	ExtendArm(Constants.ConeMidExtension));
-		// pitstick.povUp().toggleOnTrue(new ParallelCommandGroup(new PivotArm(0), new ExtendArm(4)));
-		// pitstick.povDown().toggleOnTrue(new ExtendArm(3));
-
-
-		// pitstick.povDownLeft().toggleOnTrue(new ParallelCommandGroup(new ExtendArm(0.3), new PivotArm(Constants.ConeIntakePivotBack)));
-		// pitstick.povDownRight().toggleOnTrue(new ParallelCommandGroup(new ExtendArm(0.3), new PivotArm(Constants.ConeIntakePivotFront)));
-	
-	}
-
-	private void configureBindings() {
-		// Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-		driverA = new JoystickButton(driver, XboxController.Button.kA.value);
-		driverB = new JoystickButton(driver, XboxController.Button.kB.value);
-		driverX = new JoystickButton(driver, XboxController.Button.kX.value);
-		driverY = new JoystickButton(driver, XboxController.Button.kY.value);
-
-		driverLT = new JoystickButton(driver, XboxController.Axis.kLeftTrigger.value);
-		driverLB = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-		driverRT = new JoystickButton(driver, XboxController.Axis.kRightTrigger.value);
-		driverRB = new JoystickButton(driver, XboxController.Button.kRightBumper.value);
-		driverDPadDown = new POVButton(driver, 180);
-		driverDPadUp = new POVButton(driver, 0);
-		driverDPadRight = new POVButton(driver, 90);
-		driverDPadLeft = new POVButton(driver, 270);
-		driverLeftX = new JoystickButton(driver, XboxController.Axis.kLeftX.value);
-		driverLeftY = new JoystickButton(driver, XboxController.Axis.kLeftY.value);
-		driverRightX = new JoystickButton(driver, XboxController.Axis.kRightX.value);
-		driverRightY = new JoystickButton(driver, XboxController.Axis.kRightY.value);
-		driverStart = new JoystickButton(driver, XboxController.Button.kStart.value);
-		driverBack = new JoystickButton(driver, XboxController.Button.kBack.value);
-		operatorA = operator.a();
-		operatorB = operator.b();
-		operatorY = operator.y();
-		operatorX = operator.x();
-
-		operatorDPadUp = operator.povUp();
-		operatorDPadDown = operator.povDown();
-		operatorDPadRight = operator.povRight();
-		operatorDPadLeft = operator.povLeft();
-
-		operatorLT = operator.leftTrigger(0.5);
-		operatorRT = operator.rightTrigger(0.5);
-		operatorRB = operator.rightBumper();
-		operatorLB = operator.leftBumper();
-
-		operatorLeftXPos = operator.axisGreaterThan(0, 0.4);
-		operatorLeftXNeg = operator.axisLessThan(0, -0.4);
-
-		operatorLeftYPos = operator.axisGreaterThan(1, 0.4);
-		operatorLeftYNeg = operator.axisLessThan(1, -0.4);
-
-		operatorTinyLeft = operator.back();
-		operatorTinyRight = operator.start();
-		operatorRightYPos = operator.axisGreaterThan(5, 0.3);
-		operatorRightYNeg = operator.axisGreaterThan(5, -0.3);
-
-	}
+    // Run path following command, then stop at the end.
+    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
+  }
 }
