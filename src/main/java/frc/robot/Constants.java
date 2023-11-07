8522421d392da6d5c4a46be41cd8095e4ec5a992
frcteam2791 <team2791@gmail.com>
2023-11-06 19:17:46 -0800

    Tuned PID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxSpeedMetersPerSecond = 2;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
@@ -97,14 +97,14 @@ public final class Constants {
 
     public static final double kDrivingP = 0.05; // .04
     public static final double kDrivingI = 0;
-    public static final double kDrivingD = 0;
+    public static final double kDrivingD = 0.01;
     public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
-    public static final double kTurningP = 1.5; // 1
-    public static final double kTurningI = 0;
-    public static final double kTurningD = 0;
+    public static final double kTurningP = 1; // 1
+    public static final double kTurningI = 0.001;
+    public static final double kTurningD = 0.01;
     public static final double kTurningFF = 0;
     public static final double kTurningMinOutput = -1;
     public static final double kTurningMaxOutput = 1;
