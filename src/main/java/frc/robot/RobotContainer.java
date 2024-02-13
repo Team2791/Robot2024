a9ceab6e0d3dbbdd7b72250860b9e60aa1a520fd
frcteam2791 <team2791@gmail.com>
2024-02-12 19:28:55 -0800

    cleen

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,112 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import frc.robot.constants.OIConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+	// The robot's subsystems
+	private final DriveSubsystem drivetrain = new DriveSubsystem();
+
+	// The driver's controller
+	XboxController controller = new XboxController(OIConstants.kDriverControllerPort);
+
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		// Configure the button bindings
+		configureButtonBindings();
+
+		// Configure default commands
+		drivetrain.setDefaultCommand(
+				// The left stick controls translation of the robot.
+				// Turning is controlled by the X axis of the right stick.
+				new RunCommand(
+						() -> drivetrain.drive(
+								-MathUtil.applyDeadband(controller.getLeftY(), OIConstants.kDriveDeadband),
+								-MathUtil.applyDeadband(controller.getLeftX(), OIConstants.kDriveDeadband),
+								-MathUtil.applyDeadband(controller.getRightX(), OIConstants.kDriveDeadband),
+								true, true),
+						drivetrain));
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by
+	 * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+	 * subclasses ({@link
+	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+	 * passing it to a
+	 * {@link JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+		new JoystickButton(controller, Button.kR1.value)
+				.whileTrue(new RunCommand(
+						() -> drivetrain.setX(),
+						drivetrain));
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	/**
+	 * public Command getAutonomousCommand() {
+	 * // Create config for trajectory
+	 * TrajectoryConfig config = new TrajectoryConfig(
+	 * AutoConstants.kMaxSpeedMetersPerSecond,
+	 * AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+	 * // Add kinematics to ensure max speed is actually obeyed
+	 * .setKinematics(DriveConstants.kDriveKinematics);
+	 * 
+	 * // An example trajectory to follow. All units in meters.
+	 * Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+	 * // Start at the origin facing the +X direction
+	 * new Pose2d(0, 0, new Rotation2d(0)),
+	 * // Pass through these two interior waypoints, making an 's' curve path
+	 * List.of(new Translation2d(1, 1), new Translation2d(2, 0)),
+	 * // End 3 meters straight ahead of where we started, facing forward
+	 * new Pose2d(3, 0, new Rotation2d(90)),
+	 * config);
+	 * 
+	 * PIDController xController = new PIDController(AutoConstants.kPXController, 0,
+	 * 0);
+	 * PIDController yController = new PIDController(AutoConstants.kPYController, 0,
+	 * 0);
+	 * ProfiledPIDController thetaController = new
+	 * ProfiledPIDController(AutoConstants.kPThetaController, 0, 0,
+	 * AutoConstants.kThetaControllerConstraints);
+	 * thetaController.enableContinuousInput(-Math.PI, Math.PI);
+	 * 
+	 * SwerveControllerCommand swerveControllerCommand = new
+	 * SwerveControllerCommand(exampleTrajectory,
+	 * m_robotDrive::getPose, DriveConstants.kDriveKinematics, xController,
+	 * yController, thetaController,
+	 * m_robotDrive::setModuleStates, m_robotDrive);
+	 * 
+	 * return new SequentialCommandGroup(
+	 * new InstantCommand(() ->
+	 * m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose())),
+	 * swerveControllerCommand, new InstantCommand(() ->
+	 * m_robotDrive.stopModules()));
+	 * 
+	 * }
+	 */
+}
---
+++ src/main/java/frc/robot/constants/OIConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.05;
+}
